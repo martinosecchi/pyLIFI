c4e4ef93879943d44ce195294aa18db2b964d440
Martino Secchi <smart91k@gmail.com>
2017-03-14 15:05:08 +0100

    double established by delta time + sequence alignment for comparison

--- classify.py
+++ classify.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 import sys
 import getopt
+from alignment.sequence import Sequence
+from alignment.vocabulary import Vocabulary
+from alignment.sequencealigner import SimpleScoring, GlobalSequenceAligner
 
 
 class LiFiClassifier(object):
@@ -12,16 +15,15 @@ class LiFiClassifier(object):
 		self.up = 'u'
 		self.down = 'd'
 		self.direction = self.down
-		self.count = 1
 		self.seq = self.direction
-		self.size = 0
 		self.result = ''
 		self.start = args['start'] if args.has_key('start') else 0
 		self.end = args['end'] if args.has_key('end') else None
 
 	def predict(self, wndw):
-		# wndw is a string [dir start][num=1][dir][dir end]
-		# ex: 'd3ud' -> d,u,u,u,d
+		# wndw is a string [dir start][timedelta][dir][dir end]
+		# ex: 'd300ud' -> d,u,u,u,d
+		# timedelta of 100 is 1 ms
 
 		print wndw,
 
@@ -29,29 +31,35 @@ class LiFiClassifier(object):
 		start = wndw[0]
 		end = wndw[-1]
 		wndw = wndw[1:-1]
-		count = int(''.join(filter(str.isdigit,wndw)))
+		delta = int(''.join(filter(str.isdigit,wndw)))
 		mid = wndw[-1]
+		isdoubleafter = 390
+		issingle = 180
 
 		if mid == start or mid == end:
-			count+=1
+			delta+=100
 
-		if count >= 5:
-			pred = '11' if mid == self.up else '00'
-			# pred = '11'*(count/5) if mid == self.up else '00'*(count/5) # in case no max size
+		print delta, 
+
+		if delta >= isdoubleafter:
+			# pred = '11' if mid == self.up else '00'
+			pred = '1'*(delta/issingle) if mid == self.up else '0'*(delta/issingle)
 		else:
 			pred = '1' if mid == self.up else '0'
+
 		print pred
+		
 		self.result += pred
 		self.size = 1
-		self.count = 1
 		self.seq = self.direction
 
 	def process(self, lines):
 		if not self.end:
 			self.end = float(lines[-1].split(" ")[0])
-		prev = float(lines[0].split(" ")[0])
+		prev = int(lines[0].split(" ")[1])
+		timestart = float(lines[0].split(" ")[0])
 
-		print self.epsilon
+		print 'EPSILON:',self.epsilon
 
 		for l in lines[1:]:
 			value = int(l.split(" ")[1])
@@ -59,40 +67,37 @@ class LiFiClassifier(object):
 
 			if time < float(self.start):
 				prev = value
+				timestart = time
 				continue
 			elif float(time) >= float(self.end):
 				break
 
-			print '(%f, %d)'%(time,value) , 
-			self.size += 1
-
 			# staying
 			if abs(value - prev) <= self.epsilon:
-				self.count += 1 
-				# maybe, interrupt on size > somevalue?	
+				pass
 
 			#going down
 			elif value <= prev: 
 				if self.direction == self.up:
-					self.seq += str(self.count) + self.direction + self.down
+					delta = int((time-timestart)*100)
+					self.seq += str(delta) + self.direction + self.down
 					self.predict(self.seq)
 					self.direction = self.down
-
-				elif self.direction == self.down:
-					self.count += 1
+					timestart = time
 
 			# going up
 			elif value > prev: 
 				if self.direction == self.down:
-					self.seq += str(self.count) + self.direction + self.up
+					delta = int((time-timestart)*100)
+					self.seq += str(delta) + self.direction + self.up
 					self.predict(self.seq)
 					self.direction = self.up
-
-				elif self.direction == self.up:
-					self.count += 1
+					timestart = time
 
 			prev = value
-		self.seq += str(self.count) + self.direction + self.direction
+
+		delta = int((time-timestart)*100)
+		self.seq += str(delta) + self.direction + self.direction
 		self.predict(self.seq)
 
 	def find_epsilon(self, lines):
@@ -123,29 +128,45 @@ def cut_zeroes(res):
 		i+=1
 	return res[i:]
 
-def compare(res):
-	# for comparing known transmission of "hello world"
-	# intendedbin = '0110100001100101011011000110110001101111001000000111011101101111011100100110110001100100'
-	# intended = ''.join(['01' if x == '1' else '10' for x in intendedbin])
-	intended = '10010110011010101001011010011001100101100101101010010110010110101001011001010101101001101010101010010101100101011001011001010101100101011010011010010110010110101001011010011010'
+def manchester(string):
+	s = ""
+	for c in bytearray(string):
+		for i in range(7,-1,-1):
+			s +=  "01" if (c & (1 << i)) else "10"
+	return s;
+
+
+
+def compare(res, intended='hello world'):
+
+	intended = manchester(intended)
+	print 
 	print 'intended'
 	print intended
-	print '==='
+	print 
 	print 'result'
 	print res
-	print '==='
-	res = cut_zeroes(res)
+	print 
+	
+	a = Sequence(list(res))
+	b = Sequence(list(intended))
+	v = Vocabulary()
+	aEncoded = v.encodeSequence(a)
+	bEncoded = v.encodeSequence(b)
 
-	intended = intended[3:]
-	res = res[2:]
+	# Create a scoring and align the sequences using global aligner.
+	scoring = SimpleScoring(2, -1)
+	aligner = GlobalSequenceAligner(scoring, -2)
+	score, encodeds = aligner.align(aEncoded, bEncoded, backtrace=True)
 
-	s = min(len(intended), len(res))
-	for i in xrange(s):
-		if res[i] == intended[i]:
-			print res[i],
-		else:
-			print '-',
-	print '_' * abs(len(intended) - len(res))
+	# Iterate over optimal alignments and print them.
+	for encoded in encodeds:
+	    alignment = v.decodeSequenceAlignment(encoded)
+	    print alignment
+	    print 'Alignment score:', alignment.score
+	    print 'Percent identity:', alignment.percentIdentity()
+	    print
+
 
 def help():
 	pass
@@ -169,7 +190,7 @@ def main(argv):
 		elif opt == '--epsilon':
 			model.epsilon = int(arg)
 
-	print samplefile
+	print 'FILE:',samplefile
 	f = open(samplefile, 'r')
 	lines = f.readlines()
 	f.close()
